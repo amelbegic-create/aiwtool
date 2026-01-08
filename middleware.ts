b98cfde481e819e8fd19945f0f876fda741bf514
Amel <amel.begic@mojsan.ba>
2026-01-08 17:24:23 +0100

    Hardcoded secrets in middleware and authOptions

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,14 @@ export default withAuth({
   pages: {
     signIn: "/login",
   },
+  // OVO JE KLJUČNO! Middleware mora imati svoju kopiju šifre:
+  secret: "tvoja_super_tajna_sifra_koja_sigurno_radi_123_456", 
+  
   callbacks: {
-    authorized: ({ token }) => !!token,
+    authorized: ({ token }) => {
+      // Ako postoji token, pusti korisnika
+      return !!token; 
+    },
   },
 });
 
